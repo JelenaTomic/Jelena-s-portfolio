4cdf5eb2935ffbfb563871e6601f567f892e69a4
JelenaTomic <tomic.elena09@gmail.com>
2022-10-31 15:18:31 +0100

    added contact.js and fixed images in projects

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
+import ContactForm from './components/Contact';
 
 function App() {
   return (
     <div className="App">
     <About/>
     <Portfolio/>
+    <ContactForm/>
     </div>
   );
 }
