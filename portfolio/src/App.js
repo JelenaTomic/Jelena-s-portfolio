696cf2e809d3003df360997510271a1345fd8e60
JelenaTomic <tomic.elena09@gmail.com>
2022-10-31 15:24:19 +0100

    added home.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import ContactForm from './components/Contact';
-
+import Home from './components/Home';
 function App() {
+
   return (
     <div className="App">
+    <Home/>
     <About/>
     <Portfolio/>
     <ContactForm/>
---
+++ portfolio/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+
+  return (
+    <section className="" >
+        <div className="d-flex justify-content-center"> <p className='p-5 display-5'>Jelena Tomic</p> </div>
+    
+        <div className="d-flex justify-content-center"> <p className='display-6'>Welcome To My Page!</p> </div>
+
+    </section>
+  )
+}
+
+export default Home
