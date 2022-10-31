76ca23af716a593b4b22c84cf226ef95ed6c796f
JelenaTomic <tomic.elena09@gmail.com>
2022-10-31 18:52:17 +0100

    added css

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,17 +5,36 @@ import ContactForm from './components/Contact';
 import Home from './components/Home';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
+import React, { useState } from 'react';
 
 function App() {
 
+
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <ContactForm />;
+    }
+   
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
-    <div className="App">
-    <Home/>
-    <Nav/>
-    <About/>
-    <Portfolio/>
-    <ContactForm/>
-    <Footer/>
+    <div>
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      <Footer />
     </div>
   );
 }
