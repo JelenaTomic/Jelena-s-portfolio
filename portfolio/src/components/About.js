c4d4885d2851ee36135f8c9b22e667e90c92c2f0
JelenaTomic <tomic.elena09@gmail.com>
2022-10-31 14:14:16 +0100

    added portfolio

---
+++ portfolio/src/components/About.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function About() {
+    return (
+        <section className="m-5">
+            <div className='p-5'>
+
+                <h1 id="content">About me</h1>
+            </div>
+            <div className="d-flex justify-content-center">
+                <p className='p-5 display-5'>
+                    My name is Jelena Tomic. I'm 28 years old. Born and raised in Serbia.
+
+                    I moved to the United States in mid 2018.
+
+                    I studied at the University of Belgrade and have a bachelor in traffic and transportation engineering.
+
+                    Im currently working in a tracking company as a safety director.
+
+                    While i was in college i also went to makeup school,
+                    and became first assistant to a famous Serbian makeup artist.
+
+
+                    I would say for myself that I'm an easy going person open to new adventures and always on the hunt to learn something new
+
+                    In my free time I enjoy traveling and trying different foods. I also love movies
+                    and visiting movie theaters.
+
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default About
